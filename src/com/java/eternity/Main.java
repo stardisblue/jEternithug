6fad1cf6616e817194846d9e1447b8c79af54121
Jeffou <jf.ramos0@gmail.com>
2015-12-09 14:09:36 +0100

    Test du nb noeuds des différents parcours en 4*4, 5*5 et 6*6

--- src/com/java/eternity/Main.java
+++ src/com/java/eternity/Main.java
@@ -16,17 +16,20 @@ public class Main {
     final static int MODE_SI = 2;
     final static int MODE_SO = 3;
     final static int MODE_SQ = 4;
-    final static int[][] SI4 ={{0,0},{1,0},{2,0},{3,0},{3,1} ,{3,2},{3,3},{2,3},{1,3},{0,3},{0,2},{0,1},{1,1},{2,1},{2,2},{1,2}};
-    final static int[][] SI5 ={{0,0},{1,0},{2,0},{3,0},{4,0},{4,1},{4,2},{4,3},{4,4},{3,4},{2,4},{1,4},{0,4},{0,3},{0,2},{0,1},{1,1},{1,2},{1,3},{2,3},{3,3},{3,2},{3,1},{2,1},{2,2}};
-    final static int[][] SO4 ={{1,2},{2,2},{2,1},{1,1},{0,1},{0,2},{0,3},{1,3},{2,3},{3,3},{3,2},{3,1},{3,0},{2,0},{1,0},{0,0}};
-    final static int[][] SO5 ={{2,2},{2,1},{3,1},{3,2},{3,3},{2,3},{1,3},{1,2},{1,1},{0,1},{0,2},{0,3},{0,4},{1,4},{2,4},{3,4},{4,4},{4,3},{4,2},{4,1},{4,0},{3,0},{2,0},{1,0},{0,0}};
-    final static int[][] SQ4 ={ {0,0},{3,0},{3,3},{0,3} ,{1,0},{3,1},{2,3},{0,2} ,{2,0},{3,2},{1,3},{0,1} ,{1,1},{2,1},{2,2},{1,2} };
-    final static int[][] SQ5 ={ {0,0},{4,0},{4,4},{0,4} ,{1,0},{4,1},{3,4},{0,3} ,{2,0},{4,2},{2,4},{0,2} ,{3,0},{4,3},{1,4},{0,1} ,{1,1},{3,1},{3,3},{1,3} ,{2,1},{3,2},{2,3},{1,2} ,{2,2}};
+    //final static int noeuds = 0;
+    final static int[][] SI4 = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {3, 1}, {3, 2}, {3, 3}, {2, 3}, {1, 3}, {0, 3}, {0, 2}, {0, 1}, {1, 1}, {2, 1}, {2, 2}, {1, 2}};
+    final static int[][] SI5 = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}, {0, 3}, {0, 2}, {0, 1}, {1, 1}, {1, 2}, {1, 3}, {2, 3}, {3, 3}, {3, 2}, {3, 1}, {2, 1}, {2, 2}};
+    final static int[][] SI6 = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {5, 1}, {5, 2}, {5, 3}, {5, 4}, {5, 5}, {4, 5}, {3, 5}, {2, 5}, {1, 5}, {0, 5}, {0, 4}, {0, 3}, {0, 2}, {0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {3, 4}, {2, 4}, {1, 4}, {1, 3}, {1, 2}, {2, 2}, {3, 2}, {3, 3}, {2, 3}};
+    final static int[][] SO4 = {{1, 2}, {2, 2}, {2, 1}, {1, 1}, {0, 1}, {0, 2}, {0, 3}, {1, 3}, {2, 3}, {3, 3}, {3, 2}, {3, 1}, {3, 0}, {2, 0}, {1, 0}, {0, 0}};
+    final static int[][] SO5 = {{2, 2}, {2, 1}, {3, 1}, {3, 2}, {3, 3}, {2, 3}, {1, 3}, {1, 2}, {1, 1}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {4, 3}, {4, 2}, {4, 1}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}};
+    final static int[][] SO6 = {{2, 3}, {3, 3}, {3, 2}, {2, 2}, {1, 2}, {1, 3}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {4, 3}, {4, 2}, {4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5}, {5, 5}, {5, 4}, {5, 3}, {5, 2}, {5, 1}, {5, 0}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}};
+    final static int[][] SQ4 = {{0, 0}, {3, 0}, {3, 3}, {0, 3}, {1, 0}, {3, 1}, {2, 3}, {0, 2}, {2, 0}, {3, 2}, {1, 3}, {0, 1}, {1, 1}, {2, 1}, {2, 2}, {1, 2}};
+    final static int[][] SQ5 = {{0, 0}, {4, 0}, {4, 4}, {0, 4}, {1, 0}, {4, 1}, {3, 4}, {0, 3}, {2, 0}, {4, 2}, {2, 4}, {0, 2}, {3, 0}, {4, 3}, {1, 4}, {0, 1}, {1, 1}, {3, 1}, {3, 3}, {1, 3}, {2, 1}, {3, 2}, {2, 3}, {1, 2}, {2, 2}};
 
     public static int[] calculyx(int position, int size, int mode) {
         int sum = 0;
         int pos_inverse;
-        switch(mode) {
+        switch (mode) {
             case MODE_DIAG: // diagonale
                 for (int i = 1; i <= size; i++) {
                     sum += i;
@@ -48,30 +51,34 @@ public class Main {
                 }
                 break;
             case MODE_HORI: // horizontale
-                if(position % size == 1){
-                    return new int[]{0,position/size};
-                } else if(position % size == 0) {
-                    return new int[]{size - 1,(position/size) - 1};
+                if (position % size == 1) {
+                    return new int[]{0, position / size};
+                } else if (position % size == 0) {
+                    return new int[]{size - 1, (position / size) - 1};
                 } else {
-                    return new int[]{position - size*(position/size) - 1,position/size};
+                    return new int[]{position - size * (position / size) - 1, position / size};
                 }
             case MODE_SI: // SI
-                if(size == 4){
-                    return SI4[position-1];
-                } else {
+                if (size == 4) {
+                    return SI4[position - 1];
+                } else if (size == 5) {
                     return SI5[position - 1];
+                } else {
+                    return SI6[position - 1];
                 }
             case MODE_SO: // SO
-                if(size == 4){
-                    return SO4[position-1];
+                if (size == 4) {
+                    return SO4[position - 1];
+                } else if (size == 5) {
+                    return SO5[position - 1];
                 } else {
-                    return SO5[position-1];
+                    return SO6[position - 1];
                 }
             case MODE_SQ: // SQ
-                if(size == 4){
-                    return SQ4[position-1];
+                if (size == 4) {
+                    return SQ4[position - 1];
                 } else {
-                    return SQ5[position-1];
+                    return SQ5[position - 1];
                 }
         }
         return new int[]{0, 0};
@@ -129,18 +136,24 @@ public class Main {
 
         coordonnees = new int[size * size][2];
         for (int i = 0; i < size * size; i++) {
-            System.out.println("-----------");
-            System.out.print(calculyx(i + 1, size,3)[0]);
-            System.out.println(calculyx(i + 1, size,3)[1]);
+            //System.out.println("-----------");
+            //System.out.print(calculyx(i + 1, size,MODE_SO)[0]);
+            //System.out.println(calculyx(i + 1, size,MODE_SO)[1]);
             //Affichage du parcours(coordonnées des positions dans l'ordre numérique);
-            coordonnees[i] = calculyx(i + 1, size,1);
+            coordonnees[i] = calculyx(i + 1, size, MODE_SI);
+            //MODE_DIAG
+            //MODE_HORI
+            //MODE_SI
+            //MODE_SO
+            //MODE_SQ
         }
-
+        int[] noeuds = {0};
         ArrayList<String> possibles = new ArrayList<>();
         // jeu.setPiece(pieces.remove(0), 0, 0, (byte) 1);
-        calculRecursifAmeliore(size, 1, jeu, possibles);
-        System.out.println(possibles.size());
 
+        calculRecursifAmeliore(size, 1, jeu, possibles,noeuds);
+        System.out.println("nombre de noeuds " + noeuds[0]);
+        System.out.println(possibles.size()); // Nombre de solutions possible
         String string_output = "";
         try {
             for (int i = 0; i < possibles.size(); i++) {
@@ -158,8 +171,7 @@ public class Main {
     }
 
 
-    public static void calculRecursif(int size, int position, EternityII jeu, ArrayList<String> possible)
-    {
+    public static void calculRecursif(int size, int position, EternityII jeu, ArrayList<String> possible) {
         if (position <= (size * size)) {
             int[] xy = coordonnees[position - 1];
             int col = xy[1];
@@ -171,8 +183,7 @@ public class Main {
                 for (byte j = 0; j < 4; j++) {
                     piece.setTop(j);
                     if (jeu.equalColor(piece, line, col, (byte) 0) && jeu.equalColor(piece, line, col, (byte) 1) &&
-                        jeu.equalColor(piece, line, col, (byte) 2) && jeu.equalColor(piece, line, col, (byte) 3))
-                    {
+                            jeu.equalColor(piece, line, col, (byte) 2) && jeu.equalColor(piece, line, col, (byte) 3)) {
                         Piece[][] plateau = new Piece[size][size];
                         for (int k = 0; k < size; k++)
                             System.arraycopy(jeu.getTable()[k], 0, plateau[k], 0, size);
@@ -195,8 +206,7 @@ public class Main {
         }
     }
 
-    public static void calculRecursifAmeliore(int size, int position, EternityII jeu, ArrayList<String> possible)
-    {
+    public static void calculRecursifAmeliore(int size, int position, EternityII jeu, ArrayList<String> possible, int[] noeuds) {
         if (position <= (size * size)) {
             int[] xy = coordonnees[position - 1];
             int col = xy[1];
@@ -208,12 +218,12 @@ public class Main {
                 for (byte j = 0; j < 4; j++) {
                     piece.setTop(j);
                     if (jeu.equalColor(piece, line, col, (byte) 0) && jeu.equalColor(piece, line, col, (byte) 1) &&
-                        jeu.equalColor(piece, line, col, (byte) 2) && jeu.equalColor(piece, line, col, (byte) 3))
-                    {
+                            jeu.equalColor(piece, line, col, (byte) 2) && jeu.equalColor(piece, line, col, (byte) 3)) {
+                        noeuds[0]++;
                         pieces.remove(i);
                         jeu.setPiece(piece, line, col);
 
-                        calculRecursifAmeliore(size, position + 1, jeu, possible);
+                        calculRecursifAmeliore(size, position + 1, jeu, possible, noeuds);
 
                         jeu.setPiece(null, line, col);
                         pieces.add(i, piece);
@@ -223,11 +233,11 @@ public class Main {
             jeu = null;
         } else if (position == (size * size) + 1) {
             possible.add(jeu.toString());
+            return;
         }
     }
 
-    public static void calculRecursifTriangle(int triangle, int position, EternityII jeu, ArrayList<String> possible)
-    {
+    public static void calculRecursifTriangle(int triangle, int position, EternityII jeu, ArrayList<String> possible) {
         if (position <= triangle * (triangle + 1) / 2) {
             int[] xy = coordonnees[position - 1];
             int col = xy[1];
@@ -239,8 +249,7 @@ public class Main {
                 for (byte j = 0; j < 4; j++) {
                     piece.setTop(j);
                     if (jeu.equalColor(piece, line, col, (byte) 0) && jeu.equalColor(piece, line, col, (byte) 1) &&
-                        jeu.equalColor(piece, line, col, (byte) 2) && jeu.equalColor(piece, line, col, (byte) 3))
-                    {
+                            jeu.equalColor(piece, line, col, (byte) 2) && jeu.equalColor(piece, line, col, (byte) 3)) {
                         pieces.remove(i);
                         jeu.setPiece(piece, line, col);
 
@@ -269,9 +278,9 @@ public class Main {
                 }
             }*/
 
-                EternityII jeu = createFromFile(ASSETS + "pieces_04x04 bis.txt");
-                calculBruteForce(jeu.getPieces(), 5,5);
-                jeu = null;
+            EternityII jeu = createFromFile(ASSETS + "pieces_06x06 bis.txt");
+            calculBruteForce(jeu.getPieces(), 6, 6);
+            jeu = null;
 
         } catch (IOException e) {
             e.printStackTrace();
